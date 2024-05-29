ce094ae79073ce629b3295186fdafa5e3dff315e
Marie Blakely <marieblakely@myyahoo.com>
2024-05-29 11:14:24 -0400

    update views.py to use the dog model

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
+from .models import Dog
 
 
-
-# Add the Cat class & list and view function below the imports
-class Dog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Lennox', 'german shepherd', 'Sad dog.', 4),
-  Dog('Sushi', 'poodle', 'Very fluffy.', 0),
-  Dog('Champagne', 'husky', 'Happy blue eyed puppy.', 5),
-  Dog('Santana', 'great pyranese', 'Sick all the time.', 7)
-]
 
 # Create your views here.
 def home(request):
@@ -25,4 +11,5 @@ def about(request):
   return render(request, 'about.html')
 
 def dog_index(request):
-  return render(request, 'dogs/index.html', { 'dogs': dogs })  
+  dogs = Dog.objects.all()
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
