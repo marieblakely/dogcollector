75d48db85909285a546c919bf958fdd7a1364307
Marie Blakely <marieblakely@myyahoo.com>
2024-06-06 13:33:35 -0400

    update dog_index function to display just the logged in users dogs

--- main_app/views.py
+++ main_app/views.py
@@ -16,7 +16,7 @@ def about(request):
   return render(request, 'about.html')
 
 def dog_index(request):
-  dogs = Dog.objects.all()
+  dogs = Dog.objects.filter(user=request.user)
   return render(request, 'dogs/index.html', { 'dogs': dogs })
 
 def dog_detail(request, dog_id):
